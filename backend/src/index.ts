ab1b11ab5a4d477b669d582cfc81f22d010f04d1
Berkin Koray <73588639+Berkin95@users.noreply.github.com>
2024-05-30 20:52:43 +0200

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+// GET
+// PUT
+// POST
+// DELETE
+
+app.get("/helle", (req, res, next) => {});
+
+app.listen(5000, () => console.log(`Server is running on Port ${5000}`));

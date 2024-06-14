b210cd63d93910ac680d1a4cb5779051586440ee
Berkin Koray <73588639+Berkin95@users.noreply.github.com>
2024-06-14 16:24:10 +0300

    feat: work on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Header } from "./components";
+import { Home } from "./pages";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,6 @@
 export { Header } from "./header/header";
+export { Logo } from "./logo/logo";
+export { NavigationLink } from "./navigations/navigate-to-link";
+export { CustomizedInput } from "./inputs/custom-input";
+export { TypingAnim } from "./type-animation/type-animation";
+export { Footer } from "./footer/footer";

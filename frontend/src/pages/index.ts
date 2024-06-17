58239b9a0a319c5f5ecf35635b78fb5493606da0
Berkin Koray <73588639+Berkin95@users.noreply.github.com>
2024-06-17 12:20:33 +0300

    feat: update component

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,5 @@
 export { Home } from "./home/home";
 export { Login } from "./login/login";
 export { Signup } from "./signup/signup";
-export { RoutePlanner } from "./route-planner/route-planner";
+export { RoutePlanner } from "./route-planner";
 export { NotFound } from "./not-found/not-found";
--- frontend/src/pages/route-planner/route-planner.tsx
+++ frontend/src/pages/route-planner/route-planner.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { Box, Avatar, Typography, Button, IconButton } from "@mui/material";
-
+import { Box, Avatar, Button, IconButton, Modal } from "@mui/material";
 import { IoMdSend } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
-
 import toast from "react-hot-toast";
 import { useAuth } from "../../context/auth.context";
 import {
@@ -11,7 +9,10 @@ import {
   getUserChats,
   sendChatRequest,
 } from "../../helpers/api-communication";
+import { MdInfoOutline } from "react-icons/md";
 import { ChatItem } from "../../components/chat-item/chat-item";
+
+import "./route-planner.scss";
 
 type Message = {
   role: "user" | "assistant";
@@ -21,10 +22,12 @@ type Message = {
 export function RoutePlanner() {
   const navigate = useNavigate();
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const chatEndRef = useRef<HTMLDivElement | null>(null);
 
   const auth = useAuth();
 
   const [chatMessages, setChatMessages] = useState<Message[]>([]);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const handleSubmit = async () => {
     const content = inputRef.current?.value as string;
@@ -35,11 +38,18 @@ export function RoutePlanner() {
     setChatMessages((prev) => [...prev, newMessage]);
     try {
       const chatData = await sendChatRequest(content);
-      setChatMessages((prev) => [...prev, ...chatData.chats]);
+      setChatMessages((prev) => [
+        ...prev,
+        ...chatData.chats.filter(
+          (chat: { content: string }) =>
+            !prev.some((existingChat) => existingChat.content === chat.content)
+        ),
+      ]);
     } catch (error) {
       console.error("Error sending chat request:", error);
       toast.error("Nachricht konnte nicht übermittelt werden");
     }
+    scrollToBottom();
   };
 
   const handleDeleteChats = async () => {
@@ -54,15 +64,20 @@ export function RoutePlanner() {
     }
   };
 
+  const scrollToBottom = () => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   useLayoutEffect(() => {
     if (auth?.isLoggedIn && auth.user) {
       toast.loading("Lade Chats...", { id: "loadchats" });
       getUserChats()
         .then((data) => {
-          setChatMessages([...data.chats]);
+          setChatMessages(data.chats);
           toast.success("Die Chats wurden erfolgreich geladen", {
             id: "loadchats",
           });
+          scrollToBottom();
         })
         .catch((err) => {
           console.log(err);
@@ -80,157 +95,59 @@ export function RoutePlanner() {
     }
   }, [auth, navigate]);
 
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatMessages]);
+
   if (!auth?.user) {
     return <p>No Auth</p>;
   }
 
+  const handleOpenSidebar = () => setIsSidebarOpen(true);
+  const handleCloseSidebar = () => setIsSidebarOpen(false);
+
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "1450px",
-        borderRadius: 3,
-        mx: "auto",
-        marginTop: "1rem",
-        display: "flex",
-        flexDirection: "row",
-        overflow: "scroll",
-        overflowX: "hidden",
-        overflowY: "auto",
-        scrollBehavior: "smooth",
-      }}
-    >
-      <Box
-        sx={{
-          display: { md: "flex", xs: "none", sm: "none" },
-          flex: 0.25,
-          flexDirection: "column",
-          px: "1.5rem",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            height: "60vh",
-            bgcolor: "var(--secondary-background)",
-            borderRadius: 5,
-            flexDirection: "column",
-          }}
-        >
-          <Avatar
-            sx={{
-              mx: "auto",
-              my: 2,
-              bgcolor: "var(--primary-color)",
-              color: "var(--light-text-color)",
-              border: "1px solid #ddd",
-              fontWeight: 700,
-            }}
-          >
-            {auth.user.name[0]}
-            {auth.user.name.split(" ")[1]?.[0] || ""}
-          </Avatar>
-          <Typography
-            sx={{
-              mx: "auto",
-              my: 0,
-              p: 3,
-              textAlign: "center",
-              color: "var(--text-color)",
-            }}
-          >
-            Sie sprechen mit einem speziellen Routenplanungs-ChatBOT
-          </Typography>
-          <Typography
-            sx={{
-              mx: "auto",
-              my: 0,
-              p: 3,
-              textAlign: "center",
-              color: "var(--text-color)",
-            }}
-          >
-            Sie können alle Informationen die zu einer effizienten Erstellung
-            einer optimierten Route bereitstellen.
-          </Typography>
-          <Button
-            onClick={handleDeleteChats}
-            sx={{
-              width: "200px",
-              my: "auto",
-              color: "var(--light-text-color)",
-              fontWeight: "700",
-              borderRadius: 3,
-              mx: "auto",
-              bgcolor: "var(--primary-color)",
-              ":hover": {
-                bgcolor: "var(--secondary-color)",
-              },
-            }}
-          >
-            Konversation leeren
-          </Button>
-        </Box>
+    <Box className="route-planner">
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Button onClick={handleOpenSidebar} className="open-button">
+          <MdInfoOutline size={25} /> Chat
+        </Button>
       </Box>
-      <Box
-        sx={{
-          display: "flex",
-          height: "585px",
-          flex: { md: 0.8, xs: 1, sm: 1 },
-          flexDirection: "column",
-          px: "1.75rem",
-          overflow: "scroll",
-          overflowX: "hidden",
-          overflowY: "auto",
-          scrollBehavior: "smooth",
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            borderRadius: 3,
-            mx: "auto",
-            display: "flex",
-            flexDirection: "column",
-            overflow: "scroll",
-            overflowX: "hidden",
-            overflowY: "auto",
-            scrollBehavior: "smooth",
-          }}
-        >
+      <Modal open={isSidebarOpen} onClose={handleCloseSidebar}>
+        <Box className="sidebar-modal">
+          <Box className="profile">
+            <Avatar className="avatar">
+              {auth.user.name[0]}
+              {auth.user.name.split(" ")[1]?.[0] || ""}
+            </Avatar>
+            <p className="profile-text">
+              Sie sprechen mit einem speziellen Routenplanungs-ChatBOT
+            </p>
+            <p className="profile-text">
+              Sie können alle Informationen die zu einer effizienten Erstellung
+              einer optimierten Route bereitstellen.
+            </p>
+            <Button
+              onClick={handleDeleteChats}
+              variant="outlined"
+              color="error"
+            >
+              Konversation leeren
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Box className="chat">
+        <Box className="messages">
           {chatMessages.map((chat, index) => (
             <ChatItem content={chat.content} role={chat.role} key={index} />
           ))}
+          <div ref={chatEndRef} />
         </Box>
-        <div
-          style={{
-            width: "100%",
-            borderRadius: 8,
-            backgroundColor: "var(--secondary-background)",
-            border: "1px solid var(--border-color)",
-            display: "flex",
-            margin: "auto",
-          }}
-        >
-          <input
-            ref={inputRef}
-            type="text"
-            style={{
-              width: "100%",
-              marginTop: "auto",
-              backgroundColor: "transparent",
-              padding: "30px",
-              border: "none",
-              outline: "none",
-              color: "var(--primary-color)",
-              fontSize: "20px",
-            }}
-          />
-          <IconButton
-            onClick={handleSubmit}
-            sx={{ color: "var(--primary-color)", mx: 1 }}
-          >
-            <IoMdSend color="var(--primary-color)" />
+        <div className="input-container">
+          <input ref={inputRef} type="text" className="input" />
+          <IconButton onClick={handleSubmit} className="send-button">
+            <IoMdSend />
           </IconButton>
         </div>
       </Box>
